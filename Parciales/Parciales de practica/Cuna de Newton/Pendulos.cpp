fd3b8dfb38d20ab2269a18f707e6f46737adc7e2
CristianPe1 <cpenav@unal.edu.co>
2024-04-12 12:44:28 -0500

    Added Pendulos, CunaNewton analisis

--- Parciales/Parciales de practica/Cuna de Newton/Pendulos.cpp
+++ Parciales/Parciales de practica/Cuna de Newton/Pendulos.cpp
@@ -1,19 +1,20 @@
-#include <iostream>//Libreria para imprimir en la consola
-#include <cmath>//Libreria para funciones matemáticas
+#include <iostream>
+#include <cmath>
 #include "../vector.h"
 #include "../Random64.h"
 using namespace std;
 
-//Constantes del problema físico
-const double g=9.8;
+//--------------- Constantes globales ------------
+const double g=9.8, L=12;
+const double K=1.0e4;
 
 //Número de moleculass
-const int Nx=3; 
-const int Ny=1; 
-const double Lx=60, Ly=60; 
-const int N = Nx*Ny;
+const int N = 1;
 
-const double K=1.0e4; //Elasticidad de la colisión
+//Dimensiones de la caja
+const double Lx=0, Ly=0; 
+
+
 
 //Constantes del algoritmo de integración
 const double xi=0.1786178958448091;
@@ -29,153 +30,102 @@ class Colisionador;
 //--------- Declarar las interfases de las clases---------
 class Cuerpo{
 private:
-  //Atribtuos de la clase Cuerpo
-  //vector3D r, rp, V, F; double theta, m, R, L0;
-  vector3D r, rp, F; 
-  double m, R, L0;
-  double theta, omega, alpha;
-  vector3D Vaux, Fg;
+  double xs,ys;//punto de apoyo del pendulo
+  double theta, omega, tau;
+  double m,R;
 public:
-  vector3D V;
-  void Inicie(double x0,double y0,double xp,double yp,
-	      double Vx0,double Vy0,double m0,double R0);
-  void BorreFuerza(void){F.load(0,0,0);};// Inline
-  void SumeFuerza(vector3D dF){F+=dF;};// Inline
-  void Mueva_r(double dt,double coeficiente);
-  void Mueva_V(double dt,double coeficiente);
-  void SetAlpha(double alpha0){alpha = alpha0;};
+  void Inicie(double xs, double ys, double theta0, double omega0, 
+              double m0, double R0);
+  void Mueva_theta(double dt,double coeficiente);
+  void Mueva_omega(double dt,double coeficiente);
   void Dibujese(void);
-  friend class Colisionador;
+  void BorreTorque(void){tau = 0 ;};// Inline
+  void SumeTorque(double dtau){tau+=dtau;};// Inline
+  friend class Colisionador;  
 };
 class Colisionador{
 private:
 public:
-  void CalculeTodasLasFuerzas(Cuerpo * moleculas);
-  void CalculeFuerzaEntre(Cuerpo & moleculas1,Cuerpo & moleculas2);
+  void CalculeTodosLosTorques(Cuerpo * moleculas);
+  // void CalculeTorqueEntre(Cuerpo & moleculas1,Cuerpo & moleculas2);
 };
 
 //-------Implementar las funciones de las clases------
 //------- Funciones de la clase cuerpo --------
-void Cuerpo::Inicie(double x0,double y0,double xp,double yp,
-	      double Vx0,double Vy0,double m0,double R0){
-  r.load(x0,y0,0);  V.load(Vx0,Vy0,0); m=m0; R=R0;
-  rp.load(xp,yp,0); 
-  L0 = (r - rp).norm();
+void Cuerpo::Inicie(double x0, double y0, double theta0, double omega0, 
+            double m0, double R0){
+  xs=x0; ys=y0;
+  theta=theta0; omega=omega0; 
+  m=m0; R=R0;
 }
-void Cuerpo::Mueva_r(double dt,double coeficiente){
-  // double L = (r - rp).norm();
-  // double pre = 10;
-
-  // // theta = atan2((r-rp).y(),(r-rp).x());
-
-  // // clog<<"theta"<<theta<<endl;
-  // clog<<"Vx="<<V.x()<<", Vy="<<V.y()<<endl;
-  // // Vaux.load(V.x()*cos(theta),V.y()*sin(theta),0);
-  // // clog<<"Vaux="<<Vaux<<endl;
-  // if ((L >= L0 - pre) && (L <= L0 + pre) ) {
-  //   r+=V*(coeficiente*dt);
-  //   // r+=Vaux*(coeficiente*dt);
-  // }
+#include <fstream>
+void Cuerpo::Mueva_theta(double dt,double coeficiente){
+  theta+=omega*(coeficiente*dt);
   
-  theta += omega * dt * coeficiente;
+  ofstream archivo1("PendulosTheta.txt", ios::app); // Open the file in append mode
+  archivo1 << theta << endl; // Write the value of tau to the file
+  archivo1.close(); // Close the file
+  clog<<"Theta = "<<theta<<endl;
 
-  r.load(L0*cos(theta),L0*sin(theta),0);
- 
+  ofstream archivo2("PendulosOmega.txt", ios::app); // Open the file in append mode
+  archivo2 << omega << endl; // Write the value of tau to the file
+  archivo2.close(); // Close the file
+  clog<<"Omega = "<<omega<<endl;
+}
+
+void Cuerpo::Mueva_omega(double dt,double coeficiente){
+  double I_L = 1/3.0*m*L*L, I_R = 0.5*m*R*R;
+  double I = I_L + I_R; 
+  double alpha = tau / I;
+  omega += alpha * (coeficiente * dt);
+  ofstream archivo3("PendulosAlpha.txt", ios::app); // Open the file in append mode
+  archivo3 << alpha << endl; // Write the value of tau to the file
+  archivo3.close(); // Close the file
+  clog<<"Alpha = "<<alpha<<endl;
   
 }
-void Cuerpo::Mueva_V(double dt,double coeficiente){
-
-  omega += alpha * dt * coeficiente;
-  V.load(-1*L0*omega*sin(theta),L0*omega*cos(theta),0);
-
-  // V+=F*(coeficiente*dt/m);
-  // theta = atan2((r-rp).y(),(r-rp).x());
-
-  // vector3D Faux;
-  // Faux.load(V.x()*cos(theta),V.y()*sin(theta),0);
- 
-  // V+=Faux*(coeficiente*dt);
-}
-// Dibuja un circulo de radio R en la posición del cuerpo en base al parametro t
-// t va de 0 a 7 aproximadamente 2pi
 void Cuerpo::Dibujese(void){
-  theta = atan2((r-rp).y(),(r-rp).x());
-  cout<<" , "<<r.x()<<"+"<<R<<"*cos(t),"<<r.y()<<"+"<<R<<"*sin(t) , "
-      <<rp.x()<<"+"<<L0*cos(theta)/7.0<<"*t,"<<rp.y()<<"+"<<L0*sin(theta)/7.0<<"*t";
+  double x = L*sin(theta);
+  double y = -L*cos(theta);
+  cout<<" , "<<x + xs<<"+"<<R<<"*cos(t),"<<y + ys<<"+"<<R<<"*sin(t) , "
+  <<xs<<"+"<<x/7.0<<"*t,"<<ys<<"+"<<y/7.0<<"*t";
 }
 //------- Funciones de la clase Colisionador --------
-void Colisionador::CalculeTodasLasFuerzas(Cuerpo * moleculas){
-  
-  double theta0,alpha0,L0;
+void Colisionador::CalculeTodosLosTorques(Cuerpo * moleculas){
   int i,j;
+  double theta = moleculas[0].theta;
+
   //Borro las fuerzas de todos los moleculass
   for(i=0;i<N;i++)
-    moleculas[i].BorreFuerza();
+    moleculas[i].BorreTorque();
 
   //Recorro por parejas, calculo la fuerza de cada pareja y se la sumo a los dos
   for(i=0;i<N;i++){
 
-    theta0 = moleculas[i].theta;
-    L0 = moleculas[i].L0;
-    moleculas[i].alpha = -g / L0 * sin(theta0);
+    double theta = moleculas[i].theta;
+    double m = moleculas[i].m;
 
-    for(j=0;j<i;j++)
-      CalculeFuerzaEntre(moleculas[i],moleculas[j]);
-
+    //Fuerza de la gravedad
+    double tau = -m * g * L * sin(theta);
+    moleculas[i].SumeTorque(tau);
   }
-    
-}
-
-//Calcula la fuerza entre dos moleculas
-void Colisionador::CalculeFuerzaEntre(Cuerpo & moleculas1, Cuerpo & moleculas2){
-  
-  //Calcular el vector normal
-  vector3D r21=moleculas2.r-moleculas1.r; 
-  double d=r21.norm();
-
-
-  //Determinar si hay colisión
-  double s = moleculas1.R + moleculas2.R - d;
-  
-  if(s>0){
-    double aux= K*pow(s,1.5);
-
-    //Calcular vector normal
-    vector3D n = r21*(1.0/d);
-
-    //Calcular la fuerza
-    vector3D F=n*aux;
-
-    //Sumar las fuerzas
-    
-    moleculas2.SumeFuerza(F);  moleculas1.SumeFuerza(F*(-1));
-
-  }
-
- 
 }
 
 
 //----------- Funciones Globales -----------
 //---Funciones de Animacion---
 void InicieAnimacion(void){
-  cout<<"set terminal gif animate delay 50"<<endl; 
+  cout<<"set terminal gif animate"<<endl; 
   cout<<"set output 'Pendulos.gif'"<<endl;
-  cout<<"unset key"<<endl;//No dibuja la leyenda de los datos
-  cout<<"set xrange[-20:"<<Lx + 20<<"]"<<endl;// Rango en x
-  cout<<"set yrange[-20:"<<Ly + 20<<"]"<<endl;// Rango en y
-  cout<<"set size ratio -1"<<endl; //Para que la escala en x y y sea la misma
-  cout<<"set parametric"<<endl;// Coordenadas parametricas para x y y
-  cout<<"set trange [0:7]"<<endl;//Rango del parametro t de 0 a 7 aprox 2pi
-  cout<<"set isosamples 12"<<endl;//Establece el número de muestras de t 
-  //tanto en X como en Y a 12. Esto determina cuántos puntos se calculan 
-  //y se muestran en las gráficas paramétricas. 
+  cout<<"unset key"<<endl;
+  cout<<"set xrange[-20:"<<Lx + 20<<"]"<<endl;
+  cout<<"set yrange[-20:"<<Ly + 20<<"]"<<endl;
+  cout<<"set size ratio -1"<<endl;
+  cout<<"set parametric"<<endl;
+  cout<<"set trange [0:7]"<<endl;
+  cout<<"set isosamples 12"<<endl;  
 }
-
-//---Funciones de cuadros---
-//Inicia el cuadro de la animación
-void InicieCuadro(double tiempo, double velocidad){
-    cout << "set label \'t=" << tiempo << "s    v_{pc}="<< velocidad <<"(m/s) \' at graph 0.5,0.9 center" << endl;
+void InicieCuadro(void){
     cout<<"plot 0,0 ";
     // cout<<" , "<<Lx/7<<"*t,0";        //pared de abajo
     // cout<<" , "<<Lx/7<<"*t,"<<Ly;     //pared de arriba
@@ -183,108 +133,67 @@ void InicieCuadro(double tiempo, double velocidad){
     // cout<<" , "<<Lx<<","<<Ly/7<<"*t"; //pared de la derecha
     
 }
-
-//Termina el cuadro
 void TermineCuadro(void){
     cout<<endl;
-    cout<<"unset label" << endl;
 }
 
 int main(){
   
-  Cuerpo moleculas[N+4];
+  Cuerpo moleculas[N];
   Colisionador Newton;
-  Crandom ran64(1);//Para generar números aleatorios
-  int i,ix,iy;//Variables auxiliares para los ciclos de inicializacion
-  //Parametros de la simulación
-  double m0=1; double R0=4;
-  double kT=100; 
-  //Variables auxiliares para la condición inicial
-  double dx=Lx/(Nx+1),dy=Ly/(Ny+1);//Separación entre moleculas en x y en y
-  double theta; double V0=10;//sqrt(kT/m0);//Velocidad inicial por el teorema de equipartición 
-  //de la energía
-  double x0,y0,xp,yp,Vx0,Vy0;
-  //Variables auxiliares para correr la simulacion
-  int Ncuadros=10; double t,tdibujo,dt=1e0,tmax=5,tcuadro=tmax/(Ncuadros+1); 
-  // cout<<"tmax="<<tmax<<", tcuadro="<<tcuadro<<", dt="<<dt<<endl;
-  // clog<<"Ncuadros="<<Ncuadros<<", tmax="<<tmax<<", tcuadro="<<tcuadro<<", dt="<<dt<<endl;
 
-  //Variables auxiliares para las paredes
-  double Rpared=10*Lx, Mpared=100*m0;
+  //Parametros de la simulación
+  double m0=0.1; double R0=1.5;
+  double theta0=M_PI/12,omega0=0;
+  double x0=0,y0=L;
+  double dx = 2*R0;//Separación entre moleculas 
+ 
+  //Variables auxiliares para correr la simulacion
+  int i,Ncuadros=100; 
+  double t,tdibujo,dt=1e-0,tmax=100,tcuadro=tmax/Ncuadros; 
 
   InicieAnimacion();
   
-  //INICIO
-  //Inicializar las paredes
-  //----------------(x0,y0,z0,Vx0,Vy0,Vz0,m0,R0)
-  // moleculas[N].Inicie(Lx/2,Ly+Rpared, 0,  0, 0,  0,Mpared,Rpared,0); //Pared arriba
-  // moleculas[N + 1].Inicie(Lx/2,-Rpared, 0,  0, 0,  0,Mpared,Rpared,0); //Pared abajo
-  // moleculas[N + 2].Inicie(Lx+Rpared,Ly/2, 0,  0, 0,  0,Mpared,Rpared,0); //Pared derecha
-  // moleculas[N + 3].Inicie(-Rpared,Ly/2, 0,  0, 0,  0,Mpared,Rpared,0); //Pared izquierda
+  //Inicie las moleculas
+  for(i=0;i<N;i++){
+    x0=(i+1)*dx;
+    moleculas[i].Inicie(x0,y0,theta0,omega0,m0,R0);
+  }
+ 
   
-
-  //Inicializar las moleculas
-  //Solo inicializar 
-  // for(iy=0;iy<Ny;iy++)
-  //   for(ix=0;ix<Nx;ix++){
-  //     theta=2*M_PI*ran64.r();
-  //     x0=(ix+1)*dx; y0=(iy+1)*dy; //Vx0=V0*cos(theta); Vy0=V0*sin(theta);
-  //     //----------------(x0,y0,z0,Vx0,Vy0,Vz0,m0,R0)
-  //     moleculas[iy*Nx+ix].Inicie(x0,y0, 0,Vx0,Vy0,  0,m0,R0);	
-  //   }
-  // Vx0=V0; Vy0=0;
-  // theta=M_PI/12;
-  // x0=L0*cos(theta); y0=L0*sin(theta);
-
-  for(ix=0;ix<Nx;ix++){
-      // theta=2*M_PI*ran64.r();
-      xp=(ix+1)*dx; yp=Ly;
-      x0=(ix+1)*dx; y0=0;
-      Vx0=V0*cos(theta); Vy0=V0*sin(theta);
-      //----------------(x0,y0,xp,yp,Vx0,Vy0,m0,R0)
-      moleculas[iy*Nx+ix].Inicie(x0,y0,xp,yp,Vx0,Vy0,m0,R0);
-  } 
-
-  //---------------(x0,y0,z0,Vx0,   Vy0,Vz0,m0,R0)
-  // moleculas[0].Inicie(x0, 0, 0,  0, 0.5*V0,  0,m0,1.0);
-  // moleculas[1].Inicie(x1, 0, 0,  0, 0.5*V1,  0,m1,0.5);
+ 
   //CORRO
-  double v = V0;
-  for(t=tdibujo=0;t<=tmax;t+=dt,tdibujo+=dt){
+  for(t=tdibujo=0;t<tmax;t+=dt,tdibujo+=dt){
 
     if(tdibujo>=tcuadro){
       
-      InicieCuadro(t,v);
-      for(i=0;i<N;i++) {
-        moleculas[i].Dibujese();
-        if(i == N/2) v = moleculas[i].V.norm();
-        }
+      InicieCuadro();
+      for(i=0;i<N;i++) moleculas[i].Dibujese();
       TermineCuadro();
       
       tdibujo=0;
     }
-    // cout<<moleculas[1].Getx()<<" "<<moleculas[1].Gety()<<endl;
-    // clog<<"\nTiempo: "<<t<<"\n"<<endl;
-
-    for(i=0;i<N;i++) moleculas[i].Mueva_r(dt,xi);    
-    Newton.CalculeTodasLasFuerzas(moleculas); 
-    for(i=0;i<N;i++) moleculas[i].Mueva_V(dt,Um2lambdau2);
-
-    for(i=0;i<N;i++) moleculas[i].Mueva_r(dt,chi);
-    Newton.CalculeTodasLasFuerzas(moleculas); 
-    for(i=0;i<N;i++) moleculas[i].Mueva_V(dt,lambda);
+  
     
-    for(i=0;i<N;i++) moleculas[i].Mueva_r(dt,Um2chiplusxi);
-    Newton.CalculeTodasLasFuerzas(moleculas); 
-    for(i=0;i<N;i++)moleculas[i].Mueva_V(dt,lambda);
+    for(i=0;i<N;i++) moleculas[i].Mueva_theta(dt,xi);    
+    Newton.CalculeTodosLosTorques(moleculas); 
+    for(i=0;i<N;i++) moleculas[i].Mueva_omega(dt,Um2lambdau2);
 
-    for(i=0;i<N;i++) moleculas[i].Mueva_r(dt,chi);
-    Newton.CalculeTodasLasFuerzas(moleculas); 
-    for(i=0;i<N;i++)moleculas[i].Mueva_V(dt,Um2lambdau2);
-
-    for(i=0;i<N;i++) moleculas[i].Mueva_r(dt,xi);
+    for(i=0;i<N;i++) moleculas[i].Mueva_theta(dt,chi);
+    Newton.CalculeTodosLosTorques(moleculas); 
+    for(i=0;i<N;i++) moleculas[i].Mueva_omega(dt,lambda);
     
+    for(i=0;i<N;i++) moleculas[i].Mueva_theta(dt,Um2chiplusxi);
+    Newton.CalculeTodosLosTorques(moleculas); 
+    for(i=0;i<N;i++)moleculas[i].Mueva_omega(dt,lambda);
+
+    for(i=0;i<N;i++) moleculas[i].Mueva_theta(dt,chi);
+    Newton.CalculeTodosLosTorques(moleculas); 
+    for(i=0;i<N;i++)moleculas[i].Mueva_omega(dt,Um2lambdau2);
+
+    for(i=0;i<N;i++) moleculas[i].Mueva_theta(dt,xi);
+    
+    clog<<"\nt = "<<t<<endl;
   }
   return 0;
-
 }
---
+++ Parciales/Parciales de practica/Troyanos/puntoB/plot.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x_s, y_s, x_j, y_j, t = np.genfromtxt('data.txt', unpack=True, usecols=(0, 1, 2, 3, 4)) #Se extraen los datos.
+
+plt.style.use('seaborn-v0_8')
+
+fig, axes = plt.subplots(5,1,figsize=(7, 7))
+
+
+
+
+axes[0].plot(t, x_s, '.', color='yellow', label=r"$x^{'}(t)_{S}$")
+axes[1].plot(t, y_s, '.', color='yellow', label=r"$y^{'}(t)_{S}$")
+axes[2].plot(t, x_j, '.', color='black', label=r"$x^{'}(t)_{J}$")
+axes[3].plot(t, y_j, '.', color='black', label=r"$y^{'}(t)_{J}$")
+
+axes[4].plot(x_s, y_s, ".", color="yellow", label=r"$(x',y')_{S}$")
+axes[4].plot(x_j, y_j, ".", color="black", label=r"$(x',y')_{J}$")
+
+
+
+
+
+# Se ajustan demás detalles del gráfico.
+
+for i in [0,2]:
+    axes[i].set_xlabel('t', fontsize=12)
+    axes[i].set_ylabel('x\'(t)', fontsize=12)
+    axes[i].legend(loc='upper right')
+    axes[i].grid(True, linestyle='--')
+    axes[i].set_title("x' vs. t'", fontsize=14)
+    axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+for i in [1,3]:
+    axes[i].set_xlabel('t', fontsize=12)
+    axes[i].set_ylabel('y\'(t)', fontsize=12)
+    axes[i].legend()
+    axes[i].grid(True, linestyle='--')
+    axes[i].set_title("y' vs. t", fontsize=14)
+    axes[i].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+axes[4].set_xlabel("x'", fontsize=12)
+axes[4].set_ylabel("y'", fontsize=12)
+axes[4].legend(loc='center')
+axes[4].grid(True, linestyle='--')
+axes[4].set_title("y' vs. x'", fontsize=14)
+axes[4].ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+
+
+plt.tight_layout()
+fig.savefig('plot.png')
