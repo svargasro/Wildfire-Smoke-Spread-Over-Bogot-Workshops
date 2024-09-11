c641ea952e0cd6f7b6fd89e080567a76710c97fb
svargasro <svargasro@unal.edu.co>
2024-09-11 11:27:21 -0500

    updatedVelocityAnalysis

--- Proyecto Final/Codigo/Analisis_de_Datos/Analisis_Velocidades/lectura.py
+++ Proyecto Final/Codigo/Analisis_de_Datos/Analisis_Velocidades/lectura.py
@@ -2,27 +2,42 @@
 import pandas as pd
 import numpy as np
 
-localidades = np.array(('Movil_7ma','MinAmbiente','Puente_Aranda', 'Centro_de_Alto_Rendimiento'))
+localidades = np.array(
+('Centro_de_Alto_Rendimiento',
+'Ciudad_Bolivar',
+'Colina',
+'Fontibon',
+'Guaymaral',
+'Jazmin',
+'Las_Ferias',
+'MinAmbiente',
+'Movil_7ma',
+'Puente_Aranda',
+'San_Cristobal',
+'Suba',
+'Tunal',
+'Usaquen')
+)
+
 
 #localidades = np.array(('Centro_de_Alto_Rendimiento','3'))
+print("Direcciones")
 
 for localidad in localidades:
     print(localidad)
     globaldirection = np.full(240, -1.0)
 
-
     for i in range(0,10):
-
       try:
-        df = pd.read_excel(f'./datosDireccionViento/{localidad}/{localidad}_fromGraph_18-05-2024({i}).xlsx')
+        df = pd.read_excel(f'./datosDireccionViento/{localidad}/{localidad}_fromGraph({i}).xlsx')
         print("try",i)
       except:
-        df = pd.read_excel(f'./datosDireccionViento/{localidad}/{localidad}_fromGraph_18-05-2024.xlsx')
+        df = pd.read_excel(f'./datosDireccionViento/{localidad}/{localidad}_fromGraph.xlsx')
         print("except",i)
 
       dir = np.array(df)
       trans = np.transpose(dir)
-      direction = trans[1][2:]*(np.pi/180)
+      direction = trans[1][2:]
 
       direction = np.array([np.nan if x == 'NaN' else x for x in direction], dtype=float)
       nan_indices = np.isnan(direction)
@@ -33,13 +48,13 @@ for localidad in localidades:
 
       startIndex = i*(23+1)
       finalIndex= (i+1)*(23+1)
-      print(len(direction))
-      print(len(globaldirection[startIndex:finalIndex]))
+      print(f'{len(direction)=}')
+      print(f'{len(globaldirection[startIndex:finalIndex])=}')
       globaldirection[startIndex:finalIndex] = direction
 
     np.savetxt(f'./datosTXT/direccionViento_{localidad}.txt', globaldirection)
 
-
+print("Velocidades")
 
 for localidad in localidades:
     print(localidad)
@@ -48,13 +63,14 @@ for localidad in localidades:
     for i in range(0,10):
 
       try:
-        df = pd.read_excel(f'./datosVelocidadViento/{localidad}/{localidad}_fromGraph_18-05-2024({i}).xlsx')
+        df = pd.read_excel(f'./datosVelocidadViento/{localidad}/{localidad}_fromGraph({i}).xlsx')
         print("try",i)
       except:
-        df = pd.read_excel(f'./datosVelocidadViento/{localidad}/{localidad}_fromGraph_18-05-2024.xlsx')
+        df = pd.read_excel(f'./datosVelocidadViento/{localidad}/{localidad}_fromGraph.xlsx')
         print("except",i)
 
       vel = np.array(df)
+    
       trans = np.transpose(vel)
       velocity = trans[1][2:] #*(5*np.sqrt(3)/(5148))
       velocity = np.array([np.nan if x == 'NaN' else x for x in velocity], dtype=float)
@@ -67,7 +83,8 @@ for localidad in localidades:
 
       startIndex = i*(23+1)
       finalIndex= (i+1)*(23+1)
-      print(len (globalVelocity[startIndex:finalIndex]))
+      print(f'{len(velocity)=}')
+      print(f'{len (globalVelocity[startIndex:finalIndex])}')
       globalVelocity[startIndex:finalIndex] = velocity
 
     np.savetxt(f'./datosTXT/velocidadViento_{localidad}.txt', globalVelocity)
