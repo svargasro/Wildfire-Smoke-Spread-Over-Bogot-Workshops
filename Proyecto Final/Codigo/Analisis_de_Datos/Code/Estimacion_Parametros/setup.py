61fc70d5d4a41ac3e2e8e065ccf53dec7f35800c
CristianP_1278_W <50637775+CristianPe1@users.noreply.github.com>
2024-09-19 16:57:06 +0000

    Rename pyx file and conifgured setup.py

--- Proyecto Final/Codigo/Analisis_de_Datos/Code/Estimacion_Parametros/setup.py
+++ Proyecto Final/Codigo/Analisis_de_Datos/Code/Estimacion_Parametros/setup.py
@@ -3,8 +3,8 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules=cythonize("lattice_boltzmann.pyx", compiler_directives={'language_level': "3"}),
+    ext_modules=cythonize("LB_AdvectionDiffusion.pyx", compiler_directives={'language_level': "3"}),
     include_dirs=[numpy.get_include()]
 )
 
-#python setup.pu build_ext --inplace
+#python setup.py build_ext --inplace
