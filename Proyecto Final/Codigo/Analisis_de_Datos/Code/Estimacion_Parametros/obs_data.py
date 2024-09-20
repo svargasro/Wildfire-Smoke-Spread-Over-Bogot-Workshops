688c3bfb1b956a5d6c970d539d838f8747b9b5c1
svargasro <svargasro@unal.edu.co>
2024-09-19 22:19:38 -0500

    EstimationParameters

--- Proyecto Final/Codigo/Analisis_de_Datos/Code/Estimacion_Parametros/obs_data.py
+++ Proyecto Final/Codigo/Analisis_de_Datos/Code/Estimacion_Parametros/obs_data.py
@@ -1,6 +1,20 @@
+import numpy as np
 import pandas as pd
 
-def coord_est():
-    estaciones = pd.read_csv('../../Data/estaciones_coord.txt')
+def coord_est(nombre_archivo):
+    estaciones = pd.read_csv(nombre_archivo)
     return estaciones['linealization']
+
+
+def PM_base(nombre_archivo):
+    # Leer el archivo CSV y extraer los valores numéricos
+    df = pd.read_csv(nombre_archivo, header=None)
+    
+    # Extraer los valores numéricos en formato científico
+    datos = df.iloc[:, 0].astype(float)
+    
+    # Crear el array unidimensional
+    array_unidimensional = np.array(datos.tolist())
+    
+    return array_unidimensional
 
